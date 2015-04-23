eada8ed9e883800b745174ecf62d3fabc1aa82f2
Joakim Eriksson <joakime@sics.se>
2015-04-23 16:14:19 +0200

    added some comments

--- radio-medium/java/se/sics/emul8/radiomedium/Simulator.java
+++ radio-medium/java/se/sics/emul8/radiomedium/Simulator.java
@@ -41,6 +41,11 @@ public class Simulator {
                 /* risky stuff ... */
                 try {
                     time = ((JsonObject) json.get("params")).get("time").asLong();
+                    /* handle the time elapsed here */
+                    /* -- send a set-time to all with nodes and set the emulatorsLeft to the number of connected emulator */
+                    /* -- collect all transmissions, and node/radio-changes in the event Q */
+                    /* -- for each of the OK replies update the connection's time to the current and decrease emulatorsLeft */
+                    /* -- when emulatorsLeft == 0 then process all events that are for this time period */
                     reply.add("reply","OK");
                 } catch(Exception e) {
                     reply.add("error", new JsonObject().add("desc", "failed to set time:" + e.getMessage()));
@@ -48,6 +53,9 @@ public class Simulator {
             } else {
                 reply.add("error", new JsonObject().add("desc", "only one time controller allowed"));
             }
+        } else if (command.equals("node-config-set")) {
+            /* add this client connection to the node emulator array */
+            /* and possibly create a new node */
         }
 
         try {
